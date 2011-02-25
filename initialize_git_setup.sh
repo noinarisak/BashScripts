cc48c536c9f405464d6a1f7bbd08fccc0f1d435e
Noi Narisak <noi.narisak+ror@gmail.com>
2011-02-25 05:12:13 +0000

    initial commit

---
+++ initialize_git_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# 
+# initialize_git_setup.sh - Setup the local git instance.
+#
+# requirements:
+# apt-get install git-core
+
+git config --global user.name "Noi Narisak"
+git config --global user.email noi.narisak+ror@gmail.com
+
